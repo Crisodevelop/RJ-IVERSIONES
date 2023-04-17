83d4f5df8a00c23c58a6ad12c956123516841726
Crisodevelop <56074851+Crisodevelop@users.noreply.github.com>
2023-04-17 14:50:30 -0400

    RJ INVERSIONES
    Inversiones RJ

---
+++ SistemaPrestamo/ProyectoPrestamo/Modales/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoPrestamo.Modales
+{
+    public partial class Form1 : ProyectoPrestamo.Form1
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
